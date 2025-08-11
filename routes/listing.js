218ac7d81032bc2f6c2d92508e0309783d8a3f59
Zoya-Sk <sakinask2006@gmail.com>
2025-08-11 23:21:36 +0530

    Added MVC Model

--- routes/listing.js
+++ routes/listing.js
@@ -3,18 +3,22 @@ const router = express.Router();
 const wrapAsync = require("../utils/wrapAsync.js");
 const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
-
 const listingController = require("../controllers/listings.js");
+const multer  = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 // COMPACT FORM OF INDEX AND CREATE ROUTE
 router
   .route("/")
   .get(wrapAsync(listingController.index))
-  .post(
-    isLoggedIn,
-    validateListing,
-    wrapAsync(listingController.createListing)
-  );
+  // .post(
+  //   isLoggedIn,
+  //   validateListing,
+  //   wrapAsync(listingController.createListing)
+  // );
+  .post(upload.single('listing[image]'),(req,res)=>{
+    res.send(req.file);
+  });
 
 // NEW ROUTE
 router.get("/new", isLoggedIn, listingController.renderNewForm);
