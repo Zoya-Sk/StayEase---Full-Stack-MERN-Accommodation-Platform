2c06a239bf9d2b86acb548c854f4a92314a46772
Zoya-Sk <sakinask2006@gmail.com>
2025-08-14 20:01:15 +0530

    Final Chnages in Project to Deploy

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const expressSession = require("express-session");
+const MongoStore = require("connect-mongo");
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
@@ -23,7 +24,8 @@ const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
 const userRouter = require("./routes/user.js");
 
-const MONGO_URL = 'mongodb://127.0.0.1:27017/StayEase';
+// const MONGO_URL = 'mongodb://127.0.0.1:27017/StayEase';
+const dbUrl = process.env.ATLASDB_URL;
 
 main().then(()=>{
     console.log("Connected to DB");
@@ -33,7 +35,7 @@ main().then(()=>{
 });
 
 async function main(){
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect(dbUrl);
 }
 
 
@@ -44,8 +46,21 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    crypto:{
+        secret:process.env.SECRET
+    },
+    touchAfter: 24 * 3600,
+});
+
+store.on("error",()=>{
+    console.log("ERROR in MONGO SESSION STORE", err);
+})
+
 const sessionOptions = {
-    secret: "mysupersecretcode",
+    store,
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
     cookie: {
